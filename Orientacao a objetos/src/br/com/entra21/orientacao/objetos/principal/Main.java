24a224b84e479605191f26e5ceb1376804d837aa
lucascagostinho <lucascagostinho@gmail.com>
2022-05-20 22:03:42 -0300

    Aula orientação a objetos

--- Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/Main.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/Main.java
@@ -3,13 +3,26 @@ package br.com.entra21.orientacao.objetos.principal;
 import java.util.Scanner;
 
 import br.com.entra21.orientacao.objetos.principal.aula01.classes.Aluno;
+import br.com.entra21.orientacao.objetos.principal.aula01.classes.Professor;
 
 public class Main {
 	
 	static Scanner entrada = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-
+		
+		Professor professorJava = new Professor();
+		
+		Professor professiraIngles = new Professor("Isabelle", 25);
+		
+		System.out.println("Nome dela = " +professiraIngles.getNome());
+		
+		professiraIngles.setNome("Isabelle 2");
+		
+		System.out.println("agora o nome dela = " + professiraIngles.getNome());
+		
+		
+		
 		Aluno alunoTeste = new Aluno();
 		
 		Aluno outroAluno = new Aluno();
--- Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula01/classes/README.md
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/aula01/classes/README.md
@@ -1,1 +1,12 @@
 # Aprendendo sobre Classes e objetos
+
+## Classes são formas que criam objetos
+
+> as classes podem ser chamadas de 2 formas
+> - static onde não precisa dar new para utilizar o ponto, porém somentoe itens static aparecem como opção
+> - objeto onde para acessar os atributos e metodos é necessário dar new para criar um objeto
+
+## Encapsulamento
+> pode ser visto como uma burocracia no código
+
+to go fazer o restante
