5eb148739d33137c699d32119f9c68202ce52512
lucascagostinho <lucascagostinho@gmail.com>
2022-05-24 21:50:54 -0300

    Ajustante menu do código

--- Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/Main.java
+++ Orientacao a objetos/src/br/com/entra21/orientacao/objetos/principal/Main.java
@@ -48,29 +48,6 @@ public class Main {
 
 	public static void aprenderClassesObjetos() {
 
-		Funcionario funcionario1 = new Funcionario();
-		funcionario1.setIdade((byte) 22);
-		funcionario1.setNome("Ana");
-		funcionario1.setCargoAtual("Suporte");
-		funcionario1.setSalario(2.500f);
-		System.out
-				.println("oi meu nome é: " + funcionario1.getNome() + " e tenho " + funcionario1.getIdade() + " anos");
-
-		Funcionario funcionario2 = new Funcionario("Clara", (byte) 24, "07428461995", "Desenvolvedor", 3.000f);
-		funcionario2.getCpf();
-
-		Diretor diretor1 = new Diretor();
-		diretor1.realizarApresentacao();
-
-		Diretor diretor2 = new Diretor();
-		diretor2.realizarApresentacao();
-
-		Diretor diretor3 = new Diretor("Lucas", (byte) 25);
-		diretor3.realizarApresentacao();
-
-		Diretor diretor4 = new Diretor("Teste", (byte) 15, "07428461995", "Teste", (byte) 2500);
-		diretor4.setObservacao("testte");
-
 		Professor professorJava = new Professor();
 
 		Professor professiraIngles = new Professor("Isabelle", 25);
@@ -106,6 +83,29 @@ public class Main {
 	}
 
 	public static void aprenderHeranca() {
+
+		Funcionario funcionario1 = new Funcionario();
+		funcionario1.setIdade((byte) 22);
+		funcionario1.setNome("Ana");
+		funcionario1.setCargoAtual("Suporte");
+		funcionario1.setSalario(2.500f);
+		System.out
+				.println("oi meu nome é: " + funcionario1.getNome() + " e tenho " + funcionario1.getIdade() + " anos");
+
+		Funcionario funcionario2 = new Funcionario("Clara", (byte) 24, "07428461995", "Desenvolvedor", 3.000f);
+		funcionario2.getCpf();
+
+		Diretor diretor1 = new Diretor();
+		diretor1.realizarApresentacao();
+
+		Diretor diretor2 = new Diretor();
+		diretor2.realizarApresentacao();
+
+		Diretor diretor3 = new Diretor("Lucas", (byte) 25);
+		diretor3.realizarApresentacao();
+
+		Diretor diretor4 = new Diretor("Teste", (byte) 15, "07428461995", "Teste", (byte) 2500);
+		diretor4.setObservacao("testte");
 
 	}
 
